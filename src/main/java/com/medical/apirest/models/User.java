8cdb40094c4eaedf63c89fe9441433e66b09ce3e
Osmundo Brasil Vieira <ozzybrasil2016@gmail.com>
2021-10-02 08:32:55 -0400

    Initial setup of the medical project

---
+++ src/main/java/com/medical/apirest/models/User.java
@@ -0,0 +1,52 @@
+package com.medical.apirest.models;
+
+import java.io.Serializable;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import com.sun.istack.NotNull;
+
+@Entity
+@Table(name="TB_USER")
+public class User implements Serializable{
+	
+	private static final long serialVersionUID = 1L;
+	
+	@Id
+	@GeneratedValue(strategy=GenerationType.AUTO)
+	private long id;
+	
+	@NotNull
+	private String name;
+	
+	@NotNull
+	private String email;
+	
+	
+	public long getId() {
+		return id;
+	}
+	public void setId(long id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	
+	
+
+}
